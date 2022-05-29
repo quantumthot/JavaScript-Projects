ad11e4349e17bd0b510c49e134494c8aa038ed21
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-05-29 00:21:24 -0700

    updates

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,27 @@
-alert("Sup Foos!");
+
+
+document.write("\"People who succeed have momentum. "      //This one puts together a long string
++ " The more they succeed, the more they want to succeed"
++ " and the more they find a way to succeed. "
++ " Similarly, when someone is failing, the tendency is "
++ " to get on a downward spiral that can even become a "
++ " self-fulfilling prophecy.\""
++ " -Tony Robbins");
+
+var B = " Concatenated" + " String";                         //Concatenated string
+document.write(B);                      
+
+
+var Family = "The Hatch's", Dad = "Michael", Mom = "Denice",//Variables
+Daughter = "Jade", Son = "Tyler";
+document.write(Dad);                                        //Document writes given title
+
+document.write(3+3);                                        //Math
+
+function My_First_Function() {                              //Button function
+    var str = "This is the button text!";                   //What shows upafter puching button
+    document.getElementById("Button_Text").innerHTML = str; //Putting value of variable into the
+                                                            //elementFromPoint with the "button text" id
+}
+
+window.alert("Sup!")                                        //Window that pops up whensite first loads
