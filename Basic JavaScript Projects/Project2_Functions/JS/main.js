4cbf149b26d228067694a0a1122824bc536937cf
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-05-29 21:05:37 -0700

    updates

---
+++ Basic JavaScript Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,8 @@
+function myFunction1() {                                                               
+    document.getElementById("demo1").innerHTML = "Hello, you just pushed the button!";//What shows after pushing the button
+    }
+function myFunction() {
+    var sentence = "I am learning";                                 //Concatenated sentences
+    sentence += " a lot from this book!";                           //<---------------------
+    document.getElementById("Concatenate").innerHTML = sentence;    //Sentence that shows after pushing "click me"
+}
