01da7f90ac2e790284b708739831bc03b1ba1905
quantumthot <104961668+quantumthot@users.noreply.github.com>
2022-05-30 19:35:13 -0700

    updates

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,6 +1,7 @@
+
 function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
-    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_Ride + " to ride.";
+    Can_ride = (Height < 18) ? "You are not old enough":"You are old enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to vote.";
 }
